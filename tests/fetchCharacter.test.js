9a492dbe430c594ea2cb8fd85d4a1c0d9484b74e
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-09-09 15:34:43 -0300

    Abertura de PR

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -1,6 +1,8 @@
 require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
+//  mudança para abertura de PR
+
 describe('Teste a função fetchCharacter', () => {
   it('', () => {});
 });
